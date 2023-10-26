dd3488442a17007f91f6251bcb6eee2eb6e02a1b
yohanfaj <yohan.fajerman@efrei.net>
2023-10-26 21:34:22 +0200

    please ci

--- song-recommender/src/index.js
+++ song-recommender/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
